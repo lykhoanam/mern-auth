dc9761403fb4b6108403e7f94e4b257703bacffa
lykhoanam <lykhoanamvn@gmail.com>
2026-03-01 01:37:12 +0700

    prepare deploy render

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require("path");
 const app = express();
 
 //PORT
@@ -14,5 +15,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/auth", require("./routes/api/auth"))
 app.use("/api/users", require('./routes/api/users'))
+
+// production
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from React build
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
